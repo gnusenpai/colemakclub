90fab434f1b05a81de9ef5c85ea265416d6a5175
gnusenpai <pspslimboy98@gmail.com>
2022-04-17 04:07:41 -0400

    Remove lvls from static keyboardDivs

--- logic/keyboardDivs.js
+++ logic/keyboardDivs.js
@@ -20,44 +20,44 @@ var keyboardDivs = `
 		<div class="key" id='KeyQ'><span class="letter"></span></div>
 		<div class="key" id='KeyW'><span class="letter"></span></div>
 		<div class="key" id='KeyE'><span class="letter"></span></div>
-		<div class="key lvl3" id='KeyR'><span class="letter"></span></div>
-		<div class="key lvl3" id='KeyT'><span class="letter"></span></div>
-		<div class="key lvl3" id='KeyY'><span class="letter"></span></div>
-		<div class="key lvl3" id='KeyU'><span class="letter"></span></div>
-		<div class="key lvl5" id='KeyI'><span class="letter"></span></div>
-		<div class="key lvl5" id='KeyO'><span class="letter"></span></div>
-		<div class="key lvl5" id='KeyP'><span class="letter"></span></div>
+		<div class="key" id='KeyR'><span class="letter"></span></div>
+		<div class="key" id='KeyT'><span class="letter"></span></div>
+		<div class="key" id='KeyY'><span class="letter"></span></div>
+		<div class="key" id='KeyU'><span class="letter"></span></div>
+		<div class="key" id='KeyI'><span class="letter"></span></div>
+		<div class="key" id='KeyO'><span class="letter"></span></div>
+		<div class="key" id='KeyP'><span class="letter"></span></div>
 		<div class="key" id='BracketLeft'><span class="letter"></span></div>
 		<div class="key" id='BracketRight'><span class="letter"></span></div>
 		<div class="key onepointfiveu" id='Backslash'><span class="letter"></span></div>
 	</div>
 	<div class=row>
 		<div class="key onepointsevenfiveu"><span class="letter"></span></div>
-		<div class="key lvl1" id='KeyA'><span class="letter"></span></div>
-		<div class="key lvl1" id='KeyS'><span class="letter"></span></div>
-		<div class="key lvl1" id='KeyD'><span class="letter"></span></div>
-		<div class="key lvl1 restingPosition" id='KeyF'><span class="letter"></span></div>
-		<div class="key lvl2" id='KeyG'><span class="letter"></span></div>
-		<div class="key lvl2" id='KeyH'><span class="letter"></span></div>
-		<div class="key lvl1 restingPosition" id='KeyJ'><span class="letter"></span></div>
-		<div class="key lvl1" id='KeyK'><span class="letter"></span></div>
-		<div class="key lvl1" id='KeyL'><span class="letter"></span></div>
-		<div class="key lvl1" id='Semicolon'><span class="letter"></span></div>
+		<div class="key" id='KeyA'><span class="letter"></span></div>
+		<div class="key" id='KeyS'><span class="letter"></span></div>
+		<div class="key" id='KeyD'><span class="letter"></span></div>
+		<div class="key restingPosition" id='KeyF'><span class="letter"></span></div>
+		<div class="key" id='KeyG'><span class="letter"></span></div>
+		<div class="key" id='KeyH'><span class="letter"></span></div>
+		<div class="key restingPosition" id='KeyJ'><span class="letter"></span></div>
+		<div class="key" id='KeyK'><span class="letter"></span></div>
+		<div class="key" id='KeyL'><span class="letter"></span></div>
+		<div class="key" id='Semicolon'><span class="letter"></span></div>
 		<div class="key" id='Quote'><span class="letter"></span></div>
 		<div class="key twopointtwofiveu"><span class="letter"></span></div>
 	</div>
 	<div class=row>
 		<div class="key twopointtwofiveu"><span class="letter"></span></div>
 		<div class="key" id='KeyZ'><span class="letter"></span></div>
-		<div class="key lvl6" id='KeyX'><span class="letter"></span></div>
-		<div class="key lvl6" id='KeyC'><span class="letter"></span></div>
-		<div class="key lvl4" id='KeyV'><span class="letter"></span></div>
-		<div class="key lvl4" id='KeyB'><span class="letter"></span></div>
-		<div class="key lvl4" id='KeyN'><span class="letter"></span></div>
-		<div class="key lvl4" id='KeyM'><span class="letter"></span></div>
-		<div class="key lvl5" id='Comma'><span class="letter"></span></div>
-		<div class="key lvl6" id='Period'><span class="letter"></span></div>
-		<div class="key lvl6" id='Slash'><span class="letter"></span></div>
+		<div class="key" id='KeyX'><span class="letter"></span></div>
+		<div class="key" id='KeyC'><span class="letter"></span></div>
+		<div class="key" id='KeyV'><span class="letter"></span></div>
+		<div class="key" id='KeyB'><span class="letter"></span></div>
+		<div class="key" id='KeyN'><span class="letter"></span></div>
+		<div class="key" id='KeyM'><span class="letter"></span></div>
+		<div class="key" id='Comma'><span class="letter"></span></div>
+		<div class="key" id='Period'><span class="letter"></span></div>
+		<div class="key" id='Slash'><span class="letter"></span></div>
 		<div class="key twopointsevenfiveu"><span class="letter"></span></div>
 	</div>
 	<div class=row>
@@ -107,33 +107,33 @@ var customLayout = `
 	</div>
 	<div class=row>
 		<div class="cKey onepointsevenfiveu rowStart" id='customCapsLock'><span class="letter"></span></div>
-		<div class="cKey lvl1 normalCKey" id='customKeyA'>
+		<div class="cKey normalCKey" id='customKeyA'>
 			<span class="letter"></span>
 		</div>
-		<div class="cKey lvl1 normalCKey" id='customKeyS'><span class="letter"></span></div>
-		<div class="cKey lvl1 normalCKey" id='customKeyD'><span class="letter"></span></div>
-		<div class="cKey lvl1 normalCKey restingPosition" id='customKeyF'><span class="letter"></span></div>
-		<div class="cKey lvl2 normalCKey" id='customKeyG'><span class="letter"></span></div>
-		<div class="cKey lvl2 normalCKey" id='customKeyH'><span class="letter"></span></div>
-		<div class="cKey lvl1 normalCKey restingPosition" id='customKeyJ'><span class="letter"></span></div>
-		<div class="cKey lvl1 normalCKey" id='customKeyK'><span class="letter"></span></div>
-		<div class="cKey lvl1 normalCKey" id='customKeyL'><span class="letter"></span></div>
-		<div class="cKey lvl1 normalCKey" id='customSemicolon'><span class="letter"></span></div>
+		<div class="cKey normalCKey" id='customKeyS'><span class="letter"></span></div>
+		<div class="cKey normalCKey" id='customKeyD'><span class="letter"></span></div>
+		<div class="cKey normalCKey restingPosition" id='customKeyF'><span class="letter"></span></div>
+		<div class="cKey normalCKey" id='customKeyG'><span class="letter"></span></div>
+		<div class="cKey normalCKey" id='customKeyH'><span class="letter"></span></div>
+		<div class="cKey normalCKey restingPosition" id='customKeyJ'><span class="letter"></span></div>
+		<div class="cKey normalCKey" id='customKeyK'><span class="letter"></span></div>
+		<div class="cKey normalCKey" id='customKeyL'><span class="letter"></span></div>
+		<div class="cKey normalCKey" id='customSemicolon'><span class="letter"></span></div>
 		<div class="cKey normalCKey" id='customQuote'><span class="letter"></span></div>
 		<div class="cKey twopointtwofiveu rowEnd" id='customEnter'><span class="letter"></span></div>
 	</div>
 	<div class=row>
 		<div class="cKey twopointtwofiveu rowStart"><span class="letter"></span></div>
 		<div class="cKey normalCKey" id='customKeyZ'><span class="letter"></span></div>
-		<div class="cKey lvl6 normalCKey" id='customKeyX'><span class="letter"></span></div>
-		<div class="cKey lvl6 normalCKey" id='customKeyC'><span class="letter"></span></div>
-		<div class="cKey lvl4 normalCKey" id='customKeyV'><span class="letter"></span></div>
-		<div class="cKey lvl4 normalCKey" id='customKeyB'><span class="letter"></span></div>
-		<div class="cKey lvl4 normalCKey" id='customKeyN'><span class="letter"></span></div>
-		<div class="cKey lvl4 normalCKey" id='customKeyM'><span class="letter"></span></div>
-		<div class="cKey lvl5 normalCKey" id='customComma'><span class="letter"></span></div>
-		<div class="cKey lvl6 normalCKey" id='customPeriod'><span class="letter"></span></div>
-		<div class="cKey lvl6 normalCKey" id='customSlash'><span class="letter"></span></div>
+		<div class="cKey normalCKey" id='customKeyX'><span class="letter"></span></div>
+		<div class="cKey normalCKey" id='customKeyC'><span class="letter"></span></div>
+		<div class="cKey normalCKey" id='customKeyV'><span class="letter"></span></div>
+		<div class="cKey normalCKey" id='customKeyB'><span class="letter"></span></div>
+		<div class="cKey normalCKey" id='customKeyN'><span class="letter"></span></div>
+		<div class="cKey normalCKey" id='customKeyM'><span class="letter"></span></div>
+		<div class="cKey normalCKey" id='customComma'><span class="letter"></span></div>
+		<div class="cKey normalCKey" id='customPeriod'><span class="letter"></span></div>
+		<div class="cKey normalCKey" id='customSlash'><span class="letter"></span></div>
 		<div class="cKey twopointsevenfiveu rowEnd finalKey"><span class="letter"></span></div>
 	</div>
 	<div class='row'>
